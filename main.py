9cab5ce248d05ea3e55acf87be71a38992dc8027
Shadowevil015 <64799983+Shadowevil015@users.noreply.github.com>
2025-05-20 15:39:30 +0200

    maybe done

--- main.py
+++ main.py
@@ -1,95 +1,92 @@
 from maqueen import *
 from mbrobot_plusV2 import *
 import radio
-from micrbit import i2c, pin15, pin20, pin19
+from microbit import i2c, pin15, pin20, pin19
 
 i2c.init(freq=100000, sda=pin20, scl=pin19)
 
-MOTOR_ADDRESS = 0x10
-LINE_SENSOR_REGISTER = 0x1D
-RGB_LED_PIN = pin15  
-RGB_LED_COUNT = 4 
-
-line_detected = False
-no_line_counter = 0
-NO_LINE_THRESHOLD = 20 
-
-ignore_line_detection = False
-ignore_line_start_time = 0
-IGNORE_LINE_DURATION = 750
-
-np = neopixel.NeoPixel(RGB_LED_PIN, RGB_LED_COUNT)
-
-def read_line_sensors():
-    i2c.write(MOTOR_ADDRESS, bytes([LINE_SENSOR_REGISTER]))
-    sensor_data = i2c.read(MOTOR_ADDRESS, 2)
-
-    data = sensor_data[0]
-    L1 = (data >> 0) & 1
-    L2 = (data >> 1) & 1
-    M = (data >> 2) & 1
-    R1 = (data >> 3) & 1
-    R2 = (data >> 4) & 1
-
-    return {"L1": L1, "L2": L2, "M": M, "R1": R1, "R2": R2}
-
-def line_sensor_logic(sensors):
-
-    global line_detected, no_line_counter
-
-    if sensors["L1"] == 1 or sensors["L2"] == 1 or sensors["M"] == 1 or sensors["R1"] == 1 or sensors["R2"] == 1:
-        line_detected = True
-        no_line_counter = 0
-    else:
-        no_line_counter += 1
-        if no_line_counter >= NO_LINE_THRESHOLD:
-            line_detected = False
-
-    if line_detected and not ignore_line_detection:
-        for i in range(RGB_LED_COUNT):
-            np[i] = (255, 0, 0) 
-        np.show()
-    else:
-        for i in range(RGB_LED_COUNT):
-            np[i] = (0, 0, 0) 
-        np.show()
-
-
-
 robot = Maqueen()
 radio.on()
-radio.config(address=1969383799)
+# radio.config(address=1969383799)
+radio.config(channel=1)
+
+CAR_ID = "team3"
+MOTOR_ADDRESS = 0x10
+MOTOR_LEFT = 0x00
+MOTOR_RIGHT = 0x02
+DIR_CW_FORWARD = 0
+DIR_CCW_BACKWARD = 1
+RGB_LED_COUNT = 4
+np = neopixel.NeoPixel(pin15, RGB_LED_COUNT)
+
+stunned = False
+stun_start_time = 0
+has_lightning = False
+boost_active = False
+boost_start_time = 0
+BOOST_DURATION = 3000
+
+
 
 while True:
 
-    sensors = read_line_sensors() 
-
-    line_sensor_logic(sensors)
-
-    if instruct == "go" or instruct == 'womp' and not ignore_line_detection:
-        ignore_line_detection = True
-        ignore_line_start_time = running_time()
-
     instruct = radio.receive()
+
 
     if instruct:
         instruct = instruct.replace(',', '')
         
         if instruct == 'reverse':
-            setSpeed(255)
+            setSpeed(200)
             backward()
         if instruct == 'go':
-            setSpeed(255)
+            setSpeed(200)
             forward()
         if instruct == 'stop':
             stop()
         if instruct == 'left':
-            setSpeed(150)
+            setSpeed(175)
             left()
         if instruct == 'right':
-            setSpeed(150)
+            setSpeed(175)
             right()
         if instruct == 'womp':
-            setSpeed(50)
-            forward()
+            print('womp')
+
+        if instruct.startswith("FAST:") and instruct.split(":")[1] == CAR_ID:
+                    boost_active = True
+                    boost_start_time = running_time()
+                    display.show("B")
+        elif instruct.startswith("STUN:") and instruct.split(":")[1] != CAR_ID and not stunned:
+                    stunned = True
+                    stun_start_time = running_time()
+                    stop()
+                    display.show("X")
             
+    if stunned:
+        if running_time() - stun_start_time< 5000:
+            stop()
+            for i in range(RGB_LED_COUNT): 
+                np[i] = (255, 0, 0)
+            np.show()
+        else:
+            stunned = False
+            display.clear()
+            for i in range(RGB_LED_COUNT):
+                np[i] = (0, 0, 0)
+            np.show()
+
+    if boost_active:
+        if running_time() - boost_start_time < BOOST_DURATION:
+            setSpeed(255)
+            forward()
+            for i in range(RGB_LED_COUNT):  # Flash groen tijdens boost
+                np[i] = (0, 255, 0)
+            np.show()
+        else:
+            boost_active = False
+            stop()
+            for i in range(RGB_LED_COUNT):
+                np[i] = (0, 0, 0)
+            np.show()
+    sleep(100)
